a81be0965303164ebf3aba52259d13831858d80e
akvine <redgooseman55@gmail.com>
2023-03-12 15:43:20 +0300

    init commit

---
+++ src/main/java/ru/akvine/springtrainer/SpringTrainerApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.springtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/springtrainer/SpringTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.springtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
